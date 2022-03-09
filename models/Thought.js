608cfcd5b415521a08db70399d9ff87b1702b983
diirtydog <andrew.derek.wales@gmail.com>
2022-03-09 14:07:42 -0600

    Finished up the thought and reaction schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,14 +1,16 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/format');
 
-
-const reactionSchema = new Reaction(
+const reactionSchema = new Schema(
     {
         reactionId: {
-            // use mongoose's ObjectId data type
-            // default value is set to new ObjectId
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId()
         },
         reactionBody: {
             type: String,
             required: true,
+            maxLength: 280
             // 280 character maximum
         },
         username: {
@@ -17,26 +19,42 @@ const reactionSchema = new Reaction(
         },
         createdAt: {
             type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
             // same thing as thought createdAt
         }
     }
 )
-const thoughtSchema = new Thought (
+const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
             required: true,
+            maxLength: 280
             // must be between 1 and 280 characters
         },
         createdAt: {
             type: Date,
-            // javascript date and set a getter method to format it
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
         },
         username: {
-            type: String.typeOf.Object_id,
+            type: Schema.Types.ObjectId,
+            ref: 'User',
             required: true
             // get the username of the user that created this
         },
-        reactions[reactionSchema]
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
