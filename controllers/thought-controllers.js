6c557ac95e35bc2cbbf8abbebfa3a034f28470d1
diirtydog <andrew.derek.wales@gmail.com>
2022-03-10 14:52:04 -0600

    Made the route for getting a thought by its id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -7,16 +7,28 @@ const thoughtController = {
             .catch(err => res.status(400).json(err));
     },
 
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought at this end point!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(500).json(err));
+    },
+
     addThought({ params, body }, res) {
         console.log(body);
         Thought.create(body)
-            .then(({ _id }) => {
-                return User.findOneAndUpdate(
-                    { _id: params.pizzaId },
-                    { $push: { comments: _id } },
-                    { new: true, runValidators: true }
-                );
-            })
+            // .then(({ _id }) => {
+            //     return User.findOneAndUpdate(
+            //         { _id: params.pizzaId },
+            //         { $push: { comments: _id } },
+            //         { new: true, runValidators: true }
+            //     );
+            // })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No User with this id!' });
--- routes/api/thought-route.js
+++ routes/api/thought-route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
+    getThoughtById,
     addThought,
 } = require('../../controllers/thought-controllers');
 
@@ -8,5 +9,9 @@ router
     .route('/')
     .get(getAllThoughts)
     .post(addThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
 
 module.exports = router;
